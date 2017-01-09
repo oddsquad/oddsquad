f0a96e6cf0655814fc22e4720f9b4a73344c4225
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-01-09 13:03:39 -0700

    Maybe fix coins?

--- api/myCoins.js
+++ api/myCoins.js
@@ -2,7 +2,7 @@ module.exports = function(res, data, util) {
 	util.getUser().then(function(user) {
 		data.db.any("SELECT SUM(delta) FROM coins WHERE user_id=${user}", {user: user}).then(function(results) {
 			var tr = 0;
-			if(results.length > 0) {
+			if(results.length > 0 && results[0] != null) {
 				tr = results[0].sum;
 			}
 			util.die(200, tr);
